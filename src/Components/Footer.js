c80cc13fb32b5b383a7c8918e4cbc537076a547a
Farhan <farhansyed11.3@gmail.com>
2021-12-05 05:03:10 -0800

    removed instagram/twitter handles

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,6 @@ const footer = (props) => {
     return (
         <div className="flex-1 h-16">
             <div className="flex flex-col text-center mb-4">
-                <label className="text-xs font-bold dark:text-dark-s">created by <a className="text-green-500 dark:text-dark-purple" href="https://twitter.com/dark_prgrmmr">@dark_prgrmmr</a></label>
                 <label className="text-xs font-bold dark:text-dark-s">powered by data from <a className="text-green-500 dark:text-dark-purple" href="https://iexcloud.io">IEX Cloud</a></label>
             </div>
         </div>
